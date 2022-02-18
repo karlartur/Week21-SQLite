57c37f69f556205238f6296ec1c3af77ba514c4a
karlartur <89841872+karlartur@users.noreply.github.com>
2022-02-18 11:03:38 +0200

    Add files via upload

---
+++ DataBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SQLite;
+
+
+namespace SQLiteCountries
+{
+    class DataBase
+    {
+        public SQLiteConnection myConnection;
+        public DataBase()
+        {
+            myConnection = new SQLiteConnection("Data source=SQLiteCountries.db");
+
+        }
+        public void OpenConnection()
+        {
+            if(myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+        }
+        public void ClosedConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Closed)
+            {
+                myConnection.Close();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace SQLiteCountries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadCountries();
+        }
+        private static void ReadCountries()
+        {
+            DataBase dataBaseObj = new DataBase();
+            string query = "SELECT Country.countryName AS Country, Capital.capitalName AS Capital FROM Capital, JOIN Country ON Capital.countryID = Country.rowid";
+            SQLiteCommand myCommand = new SQLiteCommand(query, dataBaseObj.myConnection);
+            dataBaseObj.OpenConnection();
+            SQLiteDataReader data = myCommand.ExecuteReader();
+            if (data.HasRows)
+            {
+                while (data.Read())
+                {
+                    Console.WriteLine($"Country: {data["Country"]}. Capital: {data["Capital"]}");
+                }
+            }
+            dataBaseObj.ClosedConnection();
+        }
+
+    }
+}
